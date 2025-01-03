74d466a44e4a416858d0965b687514d173e3f5f0
Firoz-Thapa <Gyawat.magar@gmail.com>
2025-01-03 13:31:20 +0200

    modified studentHomePage.js

--- frontend/src/pages/student/StudentHomePage.js
+++ frontend/src/pages/student/StudentHomePage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button, Switch, TextField } from '@mui/material';
+import {  Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Switch, TextField, AccordionDetails } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserDetails } from '../../redux/userRelated/userHandle';
 import { getSubjectList } from '../../redux/sclassRelated/sclassHandle';
@@ -17,58 +17,54 @@ const StudentHomePage = () => {
   }, [dispatch, currentUser._id, classID]);
 
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead sx={{ backgroundColor: '#2E7D32' }}>
-            <TableRow>
-              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Compulsory or Not</TableCell>
-              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Topic</TableCell>
-              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Learning Outcome</TableCell>
-              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Learning Outcome Information</TableCell>
-              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Credits</TableCell>
-              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Project</TableCell>
-              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Confirmed By</TableCell>
-              <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {subjectsList && subjectsList.map((subject) => (
-              <TableRow key={subject._id}>
-                <TableCell>
-                  <Switch defaultChecked={subject.isCompulsory} />
-                  {subject.isCompulsory ? 'Compulsory' : 'Not Compulsory'}
-                </TableCell>
-                <TableCell>
-                  <TextField defaultValue={subject.topic} variant="outlined" size="small" />
-                </TableCell>
-                <TableCell>
-                  <TextField defaultValue={subject.learningOutcome} variant="outlined" size="small" />
-                </TableCell>
-                <TableCell>
-                  <TextField defaultValue={subject.additionalInfo} variant="outlined" size="small" />
-                </TableCell>
-                <TableCell>
-                  <TextField defaultValue={subject.credits} variant="outlined" size="small" />
-                </TableCell>
-                <TableCell>
-                  <TextField defaultValue={subject.project} variant="outlined" size="small" />
-                </TableCell>
-                <TableCell>
-                  <TextField defaultValue={subject.confirmedBy} variant="outlined" size="small" />
-                </TableCell>
-                <TableCell>
-                  <Button variant="contained" color="success" sx={{ mr: 1 }}>Edit</Button>
-                  <Button variant="contained" color="error">Delete</Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+   
 
-      <Button variant="contained" color="primary" sx={{ mt: 2 }}>Add New Row</Button>
-    </Container>
+     
+        <AccordionDetails>
+          <TableContainer component={Paper} sx={{ borderRadius: 2, boxShadow: 3 }}>
+            <Table>
+              <TableHead sx={{ backgroundColor: '#1976d2' }}>
+                <TableRow>
+                  <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Compulsory or Not</TableCell>
+                  <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Topic</TableCell>
+                  <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Learning Outcome</TableCell>
+                  <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Learning Outcome Information</TableCell>
+                  <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Credits</TableCell>
+                  <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Project</TableCell>
+                  <TableCell sx={{ color: 'white', fontWeight: 'bold' }}>Confirmed By</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {subjectsList && subjectsList.map((subject) => (
+                  <TableRow key={subject._id} sx={{ '&:nth-of-type(odd)': { backgroundColor: '#f5f5f5' } }}>
+                    <TableCell>
+                      <Switch checked={subject.isCompulsory} disabled />
+                      {subject.isCompulsory ? 'Compulsory' : 'Not Compulsory'}
+                    </TableCell>
+                    <TableCell>
+                      <TextField defaultValue={subject.topic} variant="outlined" size="small" fullWidth />
+                    </TableCell>
+                    <TableCell>
+                      <TextField defaultValue={subject.learningOutcome} variant="outlined" size="small" fullWidth />
+                    </TableCell>
+                    <TableCell>
+                      <TextField defaultValue={subject.additionalInfo} variant="outlined" size="small" fullWidth />
+                    </TableCell>
+                    <TableCell>
+                      <TextField defaultValue={subject.credits} variant="outlined" size="small" fullWidth />
+                    </TableCell>
+                    <TableCell>
+                      <TextField defaultValue={subject.project} variant="outlined" size="small" fullWidth />
+                    </TableCell>
+                    <TableCell>
+                      <TextField defaultValue={subject.confirmedBy} variant="outlined" size="small" fullWidth />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </AccordionDetails>
   );
 };
 

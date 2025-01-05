eca8afdb9254b1c0fd48c95314ef9eed8cbdca37
Firoz-Thapa <Gyawat.magar@gmail.com>
2025-01-05 17:36:44 +0200

    Correct array comparison in StudentSubjects component

--- frontend/src/pages/student/StudentSubjects.js
+++ frontend/src/pages/student/StudentSubjects.js
@@ -34,10 +34,11 @@ const StudentSubjects = () => {
     }, [userDetails])
 
     useEffect(() => {
-        if (subjectMarks === []) {
+        if (subjectMarks.length === 0) {
             dispatch(getSubjectList(currentUser.sclassName._id, "ClassSubjects"));
         }
     }, [subjectMarks, dispatch, currentUser.sclassName._id]);
+    
 
     const handleSectionChange = (event, newSection) => {
         setSelectedSection(newSection);
